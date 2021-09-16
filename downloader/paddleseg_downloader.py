30e54d35f66f6413759f1aa027cee5093943d5aa
huzhaoyX <zhaoyangx.hu@intel.com>
2021-09-16 09:20:09 +0800

    update paddleseg_downloader code.
    ps: attach paddleseg_full.csv and paddleseg_filtered.csv.
    Signed-off-by: huzhaoyX <zhaoyangx.hu@intel.com>

--- downloader/paddleseg_downloader.py
+++ downloader/paddleseg_downloader.py
@@ -1,128 +1,193 @@
+import argparse
 import os
 import sys
 import re
+import logging
 import csv
 from pathlib import Path
 from bs4 import BeautifulSoup
 import markdown
 import requests
 
-from common import PDModelInfo
-import base_downloader
+from downloader_base import PDFiltedModelInfo, PDAllModelInfo, base_downloader
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
-class paddleseg_downloader(base_downloader.base_downloader):
-    def __init__(self, homepage, filter_data_file, bool_download, result_save_path):
-        super().__init__(homepage, filter_data_file, bool_download, result_save_path)
 
-    def get_markdown_file_list(self):
-        if not os.path.exists(self.homepage):
-            print('ERROR: {} directory no exist.'.format(self.homepage))
-            
-        self.md_list = Path(self.homepage).glob('**/*.md')
-        return self.md_list
+class paddleseg_downloader(base_downloader):
+    def __init__(self, project_dir, filter_data_file, download_mode, models_download_path, full_model_info_save_file, filtered_model_info_save_file):
+        super().__init__(project_dir, filter_data_file, download_mode, models_download_path, full_model_info_save_file, filtered_model_info_save_file)
+        self.sum_yml_and_yaml_list = []
 
-    def get_filter_list_from_data_file(self, data_file):
-        self.all_filters = []
-        with open(data_file, newline='') as csvfile:
+    def merge_yml_list_and_yaml_list(self):
+        """
+        merge the self.all_yaml_file_list and the self.all_yml_file_list
+        to one self.sum_yml_and_yaml_list
+        """
+        if len(self.sum_yml_and_yaml_list) > 0:
+            return self.sum_yml_and_yaml_list
+
+        for yaml_file in self.all_yaml_file_list:
+            self.sum_yml_and_yaml_list.append(yaml_file)
+
+        for temp_file_name in self.all_yml_file_list:
+            self.sum_yml_and_yaml_list.append(temp_file_name)
+
+        return self.sum_yml_and_yaml_list
+
+    def special_link_detect(self, filter_str):
+        """
+        detect some special link for filter_str, if find return True, else return False
+        """
+        script_file_name = os.path.join(self.project_dir, "contrib/HumanSeg/export_model/download_export_model.py")
+        script_file = open(script_file_name, 'r') 
+        pattern = re.compile(r".*http.*.zip")
+        for line in script_file:
+            if pattern.match(line):
+                line = line.replace('"','')
+                line = line.replace(' ','')
+                line = line.replace('\n','')
+                line = line.replace(',','')
+                config_base = os.path.basename(line)
+                config_base = os.path.splitext(config_base)[0]
+                config_base = config_base.replace('_with_softmax','')
+                pattern_two = re.compile(r".*%s.*" %config_base)
+                if pattern_two.match(filter_str):
+                    config_yaml = ''.join(filter_str.split()) # remove all whitespace'
+                    self.all_model_info_list.append(PDAllModelInfo(str(script_file_name), filter_str, line))
+                    return True
+        return False 
+
+    def get_all_model_info_list(self):
+        """
+        match self.all_yml_file_list and self.all_yaml_file_list with self.all_pdparams_urls,
+        then get the self.all_model_info_list
+        """
+        if len(self.all_model_info_list) > 0:
+            return self.all_model_info_list
+
+        self.merge_yml_list_and_yaml_list()
+
+        for temp_file_name in self.sum_yml_and_yaml_list:
+            config_base = os.path.basename(temp_file_name)
+            config_base = os.path.splitext(config_base)[0]
+            pattern_one = re.compile(r".*/%s.pdparams" %config_base)
+            pattern_two = re.compile(r".*/%s/model.pdparams" %config_base)
+            count = 0
+            match_url_list = []
+            for key in self.all_pdparams_urls:
+                pdparams_url = str(key)
+                md_filename_path = self.all_pdparams_urls[key]
+                if pattern_one.match(pdparams_url) or pattern_two.match(pdparams_url):
+                    count = count + 1
+                    match_url_list.append([md_filename_path, pdparams_url])
+
+            if count >0:
+                self.all_model_info_list.append(PDAllModelInfo(str(match_url_list[0][0]), str(temp_file_name), str(match_url_list[0][1])))
+            else:
+                if not self.special_link_detect(str(temp_file_name)):
+                    self.all_model_info_list.append(PDAllModelInfo('None', str(temp_file_name), 'None'))
+
+        return self.all_model_info_list
+
+    def get_filted_model_info_list(self):
+        """
+        use self.filter_data_file info to filter the self.all_model_info_list,
+        then get the self.filted_model_info_list
+        """
+        if len(self.filted_model_info_list) > 0:
+            return self.filted_model_info_list
+
+        with open(self.filter_data_file, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in reader:
                 config_yaml = row[1]
                 config_yaml = ''.join(config_yaml.split()) # remove all whitespace
-                config_base = os.path.basename(config_yaml)
+                model_type = row[0]
+                model_type = ''.join(model_type.split()) # remove all whitespace
+                pattern = re.compile(r".*%s" %str(config_yaml))
+                match_url_list = []
+                for item in self.all_model_info_list:
+                    if pattern.match(str(item.pdconfig)):
+                        match_url_list.append([item.pdconfig, item.pdparams])
+
+                if len(match_url_list) >0:
+                    self.filted_model_info_list.append(PDFiltedModelInfo(str(model_type), str(config_yaml), str(match_url_list[0][1])))
+                else:
+                    self.filted_model_info_list.append(PDFiltedModelInfo(str(model_type), str(config_yaml), 'None'))
+
+        return self.filted_model_info_list
+
+    def download_models(self):
+        """
+        download models to models_download_path according to the self.download_mode.
+        """
+        if self.download_mode == 'None':
+            return True
+        elif self.download_mode == 'all':
+            pattern = re.compile(r".*http.*.zip")
+            for item in self.all_model_info_list:
+                if item.pdparams == 'None':
+                    continue
+                config_base = os.path.basename(item.pdconfig)
                 config_base = os.path.splitext(config_base)[0]
-                self.all_filters.append(config_base)
+                if pattern.match(str(item.pdparams)):
+                    file_name = config_base + '.zip'
+                else:
+                    file_name = config_base + '.pdparams'
+                self.download_pdparams_file(file_name, item.pdparams, self.models_download_path)
+        elif self.download_mode == 'filtered':
+            pattern = re.compile(r".*http.*.zip")
+            for item in self.filted_model_info_list:
+                if item.pdparams == 'None':
+                    continue
+                config_base = os.path.basename(item.pdconfig)
+                config_base = os.path.splitext(config_base)[0]
+                if pattern.match(str(item.pdparams)):
+                    file_name = config_base + '.zip'
+                else:
+                    file_name = config_base + '.pdparams'
+                self.download_pdparams_file(file_name, item.pdparams, self.models_download_path)
+        else:
+            return False
 
-    def get_all_pdparams_info_by_markdown_file(self):
-        self.get_filter_list_from_data_file(self.filter_data_file)
-        for md_file in self.md_list:
-            with open(md_file, 'r') as f:
-                text = f.read()
-                html_text = markdown.markdown(text)
-
-                soup = BeautifulSoup(html_text, 'html.parser')
-
-                tracks_pdparams = soup.find_all('a', attrs={'href': re.compile(r'\.pdparams$')})
-                if len(list(tracks_pdparams))>0:
-                    for track in tracks_pdparams:
-                        pdparams_url = '{}'.format(track['href'])
-                        #print(pdparams_url)
-                        for filter_text in self.all_filters:
-                            pattern = re.compile(r".*%s.*" %filter_text)
-                            if pattern.match(pdparams_url):
-                                self.all_pdparams_urls_filtered.add((filter_text, pdparams_url))
-                                # print(filter_text, pdparams_url)
-        return self.all_pdparams_urls_filtered
-
-
-    def download_pdparams_file(self, file_name,  pdparams_url):
-        if not os.path.exists(self.result_save_path):
-            os.makedirs(self.result_save_path)
-
-        if self.bool_download:
-            file_name = os.path.join(self.result_save_path, file_name)
-            # print(file_name)
-            print('Downloading: {} {}'.format(file_name, pdparams_url))
-            # Download the track
-            r = requests.get(pdparams_url, allow_redirects=True)
-            with open(file_name, 'wb') as f:
-                f.write(r.content)
+        return True
 
 
-    def pdparams_filter_and_download(self):
-        with open(self.filter_data_file, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            #combine
-            for row in reader:
-                count = 0
-                for (filter_text, pdparams_url) in self.all_pdparams_urls_filtered:
-                    pattern = re.compile(r".*%s.*" %filter_text)
-                    if pattern.match(row[1]):
-                        count = count + 1
-                        config_yaml = row[1]
-                        config_yaml = ''.join(config_yaml.split()) # remove all whitespace'
-                        self.models.append(PDModelInfo(row[0], config_yaml, pdparams_url))
-                        #download
-                        file_name = '{}.pdparams'.format(filter_text)
-                        if count > 1:
-                            file_name = '{}_{}.pdparams'.format(filter_text, count)
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--project_dir", type=str, default='../../PaddleSeg', help="PaddleSeg project directory")
+    parser.add_argument("--filter_data_file", type=str, default='../data/paddleseg.csv', help="The file we used to filter the model link")
+    parser.add_argument("--download_mode", type=str, default='None', choices=['all', 'filtered', 'None'], help="Download mode, all: download all models, filtered: only download the filtered models, None: download nothing")
+    parser.add_argument("--models_download_path", type=str, default='./paddleseg', help="where to store the downloaded models")
+    parser.add_argument("--full_model_info_save_file", type=str, default='./paddleseg_full.csv', help="The file to save the full model link info")
+    parser.add_argument("--filtered_model_info_save_file", type=str, default='./paddleseg_filtered.csv', help="The file to save the filtered model link info")
+    return parser.parse_args()
 
-                        self.download_pdparams_file(file_name, pdparams_url)
+def convert_params_to_abspath(args):
+    args.project_dir = os.path.abspath(args.project_dir)
+    args.filter_data_file = os.path.abspath(args.filter_data_file)
+    args.models_download_path = os.path.abspath(args.models_download_path)
+    args.full_model_info_save_file = os.path.abspath(args.full_model_info_save_file)
+    args.filtered_model_info_save_file = os.path.abspath(args.filtered_model_info_save_file)
 
-                if not count:
-                    script_file_name = os.path.join(self.homepage, "contrib/HumanSeg/export_model/download_export_model.py")
-                    print(script_file_name)
-                    script_file = open(script_file_name, 'r') 
-                    pattern = re.compile(r".*http.*.zip")
-                    for line in script_file:
-                        if pattern.match(line):
-                            line = line.replace('"','')
-                            line = line.replace(' ','')
-                            line = line.replace('\n','')
-                            line = line.replace(',','')
-                            config_base = os.path.basename(line)
-                            config_base = os.path.splitext(config_base)[0]
-                            config_base = config_base.replace('_with_softmax','')
-                            pattern_two = re.compile(r".*%s.*" %config_base)
-                            if pattern_two.match(row[1]):
-                                config_yaml = ''.join(row[1].split()) # remove all whitespace'
-                                self.models.append(PDModelInfo(row[0], config_yaml, line))
-                                file_name = '{}.zip'.format(config_base)
-                                self.download_pdparams_file(file_name, line)
-        #write file
-        result_file_name = os.path.join(__dir__, "paddleseg_full.csv")
-        with open(result_file_name, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerows(self.models)
+def welcome_info(args):
+    logging.info("paddle segmention models downloader begin.")
+    logging.debug("args.project_dir: {}".format(args.project_dir))
+    logging.debug("args.filter_data_file: {}".format(args.filter_data_file))
+    logging.debug("args.download_mode: {}".format(args.download_mode))
+    logging.debug("args.models_download_path: {}".format(args.models_download_path))
+    logging.debug("args.full_model_info_save_file: {}".format(args.full_model_info_save_file))
+    logging.debug("args.filtered_model_info_save_file: {}".format(args.filtered_model_info_save_file))
 
-        return self.models
+def main():
+    #logging.basicConfig(format='[%(levelname)s]-[%(filename)s:%(lineno)s] %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='[%(levelname)s]-[%(filename)s:%(lineno)s] %(message)s', level=logging.INFO)
+    args = parse_args()
+    convert_params_to_abspath(args)
+    welcome_info(args)
+    downloader = paddleseg_downloader(args.project_dir, args.filter_data_file, args.download_mode, args.models_download_path, args.full_model_info_save_file, args.filtered_model_info_save_file)
+    downloader.run()
 
 if __name__ == "__main__":
-    seg_repo_path = os.path.abspath(os.path.join(__dir__, '../../PaddleSeg'))
-    bool_download = 1
-
-    filter_file_path = os.path.join(__dir__, "../data/paddleseg.csv")
-    result_path_save_path = os.path.join(__dir__, "./paddleseg")
-
-    downloader = paddleseg_downloader(seg_repo_path, filter_file_path, bool_download, result_path_save_path)
-    downloader.run()
+    main()
+    logging.info("paddle segmention models downloader done.")
