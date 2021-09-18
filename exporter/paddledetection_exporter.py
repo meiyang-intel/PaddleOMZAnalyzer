e2459dbfc05d831d44d18c9894098336333e5642
huzhaoyX <zhaoyangx.hu@intel.com>
2021-09-19 03:19:51 +0800

    Add argparse for paddleclas_exporter.py.
    Add argparse for paddledetection_exporter.py.
    Add argparse for paddleseg_exporter.py.
    
    Signed-off-by: huzhaoyX <zhaoyangx.hu@intel.com>

--- exporter/paddledetection_exporter.py
+++ exporter/paddledetection_exporter.py
@@ -4,6 +4,8 @@ import subprocess
 import re
 import csv
 import pathlib
+import argparse
+import logging
 
 # reference:
 # https://github.com/PaddlePaddle/PaddleDetection/blob/release/2.1/deploy/EXPORT_MODEL.md
@@ -18,44 +20,88 @@ import pathlib
 # https://github.com/PaddlePaddle/PaddleDetection/blob/f0a30f3ba6095ebfdc8fffb6d02766406afc438a/docs/tutorials/PrepareDataSet.md#COCO%E6%95%B0%E6%8D%AE
 # to download coco dataset, which is required to export configs/keypoint/higherhrnet/higherhrnet_hrnet_w32_640.yml.
 
-invalidate_cache = False # default
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-if __name__ == '__main__':
-    __dir__ = os.path.dirname(os.path.abspath(__file__))
-    omz_dir = os.path.abspath(os.path.join(__dir__, '../../PaddleDetection'))
-    sys.path.append(omz_dir)  # OMZ PaddleDetection
-    os.chdir(omz_dir)
+    default_project_dir =os.path.abspath(os.path.join(__dir__, '../../PaddleDetection'))
+    default_config_file =os.path.abspath(os.path.join(__dir__, '../downloader/paddledet_filtered.csv'))
+    default_dynamic_models_path =os.path.abspath(os.path.join(__dir__, '../downloader/paddledet'))
+    default_static_models_save_path =os.path.abspath(os.path.join(__dir__, './paddledet'))
+
+    parser.add_argument("--project_dir", type=str, default=default_project_dir, help="PaddleDetection project directory")
+    parser.add_argument("--config_file", type=str, default=default_config_file, help="The file we used to specify models need to export")
+    parser.add_argument("--dynamic_models_path", type=str, default=default_dynamic_models_path, help="where to find the dynamic models")
+    parser.add_argument("--static_models_save_path", type=str, default=default_static_models_save_path, help="where to save the static models we exported")
+    parser.add_argument("--invalidate_cache", type=bool, default=False, help="if ignore the cache or not, program will reexport the dynamic models even it have been exported before when you set it True value.")
+
+    return parser.parse_args()
+
+def convert_params_to_abspath(args):
+    cwd = os.getcwd()
+    if not os.path.isabs(args.project_dir):
+        args.project_dir = os.path.abspath(os.path.join(cwd, args.project_dir))
+    if not os.path.isabs(args.config_file):
+        args.config_file = os.path.abspath(os.path.join(cwd, args.config_file))
+    if not os.path.isabs(args.dynamic_models_path):
+        args.dynamic_models_path = os.path.abspath(os.path.join(cwd, args.dynamic_models_path))
+    if not os.path.isabs(args.static_models_save_path):
+        args.static_models_save_path = os.path.abspath(os.path.join(cwd, args.static_models_save_path))
+
+def welcome_info(args):
+    logging.info("paddle detection models exporter begin.")
+    logging.debug("args.project_dir: {}".format(args.project_dir))
+    logging.debug("args.config_file: {}".format(args.config_file))
+    logging.debug("args.dynamic_models_path: {}".format(args.dynamic_models_path))
+    logging.debug("args.static_models_save_path: {}".format(args.static_models_save_path))
+
+
+def main():
+    #logging.basicConfig(format='[%(levelname)s]-[%(filename)s:%(lineno)s] %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='[%(levelname)s]-[%(filename)s:%(lineno)s] %(message)s', level=logging.INFO)
+    args = parse_args()
+    convert_params_to_abspath(args)
+    welcome_info(args)
+
+    sys.path.append(args.project_dir)  # OMZ PaddleDetection
+    os.chdir(args.project_dir)
 
     pipes = []
 
-    with open(os.path.abspath(os.path.join(__dir__, '../downloader/paddledet_filtered.csv')), newline='') as csvfile:
+    with open(args.config_file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
+            pdparams_url = row[2]
+            pdparams_url = ''.join(pdparams_url.split()) # remove all whitespace
+            if pdparams_url == 'None':
+                continue
+
             config_yaml = row[1]
             config_yaml = ''.join(config_yaml.split()) # remove all whitespace
 
-            pdparams_url = row[2]
-            output_path = os.path.abspath(os.path.join(__dir__, 'paddledet')) # paddledetection will append modelname as its exported dirname.
-
             # 'static/configs' are handled in its relative path
             p = pathlib.Path(config_yaml)
-            work_dir = os.path.realpath(os.path.join(omz_dir, 'static')) if p.parts[0]=='static' else omz_dir
+            work_dir = os.path.realpath(os.path.join(args.project_dir, 'static')) if p.parts[0]=='static' else args.project_dir
             if p.parts[0]=='static': config_yaml = str(pathlib.Path(*p.parts[1:]))  # remove 'static' from relative path
 
-            if invalidate_cache is False:
+            if args.invalidate_cache is False:
                 config_base = os.path.basename(config_yaml)
                 config_base = os.path.splitext(config_base)[0]    
-                model_path = os.path.join(output_path, config_base)
+                model_path = os.path.join(args.static_models_save_path, config_base)
                 if os.path.exists(model_path) and (os.path.exists(os.path.join(model_path, 'model.pdmodel')) or os.path.exists(os.path.join(model_path, '__model__'))): # cached
-                    print('INFO: {} already exported. Ingnore it this time.'.format(config_base))
+                    logging.info('INFO: {} already exported. Ingnore it this time.'.format(config_base))
                     continue
             
             # TODO: it might consumes all cpu/memory resources, so we'd better limit the size of concurrent working pipes.
             exporter_cmd = 'chdir {} && python3 tools/export_model.py -c {} -o use_gpu=false weights={} TestReader.inputs_def.image_shape=[3,608,608] --output_dir {}'.format(
-                work_dir, config_yaml, pdparams_url, output_path)
-            print(exporter_cmd)
+                work_dir, config_yaml, pdparams_url, args.static_models_save_path)
+            logging.info(exporter_cmd)
             p = subprocess.Popen(exporter_cmd, shell=True)
             pipes.append(p)
 
     exit_codes = [p.wait() for p in pipes]
-    print(exit_codes) #TODO: check failure
+    logging.info(exit_codes) #TODO: check failure
+
+if __name__ == '__main__':
+    main()
+    logging.info("paddle detection models exporter end.")
