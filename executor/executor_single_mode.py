79f63cbea7214b332dfb1d1aebda038677155613
jialipen <cecilia.peng@intel.com>
2021-10-14 21:54:19 +0800

    code refactor

--- executor/executor_single_mode.py
+++ executor/executor_single_mode.py
@@ -1,5 +1,93 @@
 import argparse
-from executor_class import performance_and_accuracy_test_single_mode
+import os
+
+from executor import OpenvinoExecutor, PaddlenlpPredictor, PaddleExecutor, PaddlePredictorExecutor, compare
+
+def performance_and_accuracy_test_single_mode(model_file, batch_size=1, warmup=0, repeats=1, predictor_type: str='normal', openvino_api_type: str = 'sync'):
+    try:
+        paddle_warmup_frame_time = None
+        paddle_repeats_per_frame_time = None
+        openvino_warmup_frame_time = None
+        openvino_repeats_per_frame_time = None
+        status = 0
+        if not os.path.exists(model_file):
+            print('model file "{}" not exists. Please specify it with --model_file argument.'.format(model_file))
+            return
+
+        # get the pdiparams file path
+        dir_name = os.path.dirname(model_file)
+        config_base = os.path.basename(model_file)
+        config_base = os.path.splitext(config_base)[0]
+        model_params_file = dir_name + '/' + config_base + '.pdiparams'
+
+        if not os.path.exists(model_params_file):
+            print('model params file "{}" not exists. Please check it.'.format(model_params_file))
+            return
+
+        # create test data
+        if predictor_type == 'nlp':
+            pdpd_predict_executor = PaddlenlpPredictor(str(model_file), str(model_params_file))
+            test_inputs = pdpd_predict_executor.generate_inputs(batch_size, language='English')
+        elif predictor_type == 'normal':
+            pdpd_executor = PaddleExecutor(model_file)
+            test_inputs = pdpd_executor.generate_inputs(batch_size)
+            pdpd_predict_executor = PaddlePredictorExecutor(str(model_file), str(model_params_file))
+        else:
+            print('have no this {} paddle predictor type. Please check it.'.format(predictor_type))
+
+        ## paddle predictor inference
+        pdpd_predict_executor.run(test_inputs, warmup, repeats)
+        pdpd_predict_result = pdpd_predict_executor.get_inference_results()
+        paddle_warmup_frame_time = "{:.2f}".format(pdpd_predict_executor.warmup_time) if pdpd_predict_executor.warmup_time is not None else 'None'
+        paddle_repeats_per_frame_time = "{:.2f}".format(pdpd_predict_executor.repeat_time) if pdpd_predict_executor.repeat_time is not None else 'None'
+
+        ## openvino inference
+        ov_executor = OpenvinoExecutor(model_file, 10, openvino_api_type)
+        ov_executor.run(test_inputs, warmup, repeats)
+        ov_result = ov_executor.get_inference_results()
+        openvino_warmup_frame_time = "{:.2f}".format(ov_executor.warmup_time) if ov_executor.warmup_time is not None else 'None'
+        openvino_repeats_per_frame_time = "{:.2f}".format(ov_executor.repeat_time) if ov_executor.repeat_time is not None else 'None'
+        status = 2
+
+        # compare
+        ov_result = [ov_result[k] for k in sorted(ov_result)]
+        pdpd_predict_result = [pdpd_predict_result[k] for k in sorted(pdpd_predict_result)]
+        res = compare(ov_result, pdpd_predict_result)
+
+        if res:
+            status = 3
+        else:
+            status = 4
+
+    finally:
+        print('[result:] model_file: {}'.format(model_file))
+        if status == 0:
+            print('[result:] pdpd_predict_executor inference failed.')
+
+        if status == 1:
+            print('[result:] pdpd_predict_executor inference success.')
+            print('[result:] ov_executor inference failed.')
+
+        if status == 2:
+            print('[result:] pdpd_predict_executor inference success.')
+            print('[result:] ov_executor inference success.')
+            print('[result:] compare failed.')
+
+        if status == 3:
+            print('[result:] pdpd_predict_executor inference success.')
+            print('[result:] ov_executor inference success.')
+            print('[result:] compare result equal.')
+
+        if status == 4:
+            print('[result:] pdpd_predict_executor inference success.')
+            print('[result:] ov_executor inference success.')
+            print('[result:] compare result not equal.')
+
+        print('[result:] paddle warmup average time: {}(ms)'.format(paddle_warmup_frame_time))
+        print('[result:] paddle repeates average time: {}(ms)'.format(paddle_repeats_per_frame_time))
+        print('[result:] openvino warmup average time: {}(ms)'.format(openvino_warmup_frame_time))
+        print('[result:] openvino repeates average time: {}(ms)'.format(openvino_repeats_per_frame_time))
+
 
 def main():
     args = parse_args()
--- executor/pytest_executor.py
+++ executor/pytest_executor.py
@@ -1,10 +1,75 @@
 import pytest
-from executor_class import performance_and_accuracy_test_by_params
 from collections import namedtuple
 import os
 import csv
 import argparse
 from pathlib import Path
+
+from executor import OpenvinoExecutor, PaddlenlpPredictor, PaddleExecutor, PaddlePredictorExecutor, compare
+def performance_and_accuracy_test_by_params(result_prefix_str, model_file, model_params_file, result_save_file,  result_level=1, batch_size=1, warmup=0, repeats=1, openvino_api_type: str = 'sync'):
+    '''
+    'result_level': 0: analyzer failed
+                    1: analyzer success
+                    2: openvino inference success
+                    3: paddle inference result equal to the result of openvino inference
+    writefile row: 'result_prefix_str result_level paddle_warmup_frame_time paddle_repeats_per_frame_time openvino_warmup_frame_time openvino_repeats_per_frame_time'
+    '''
+    # ResultInfo=namedtuple('ResultInfo', 'result_prefix_str result_level paddle_first_frame_time paddle_repeats_per_frame_time openvino_first_frame_time openvino_repeats_per_frame_time')
+    result_level_dict = dict(zip(range(0,4),
+                                ['op mapper failed', 'all ops mapped', 'openvino infered success', 'infer accuracy pass']))
+
+    try:
+        paddle_warmup_frame_time = None
+        paddle_repeats_per_frame_time = None
+        openvino_warmup_frame_time = None
+        openvino_repeats_per_frame_time = None
+        # No file or multi file
+        if model_file == None or model_params_file == None:
+            return
+
+        # create test data
+        pdpd_executor = PaddleExecutor(model_file)
+        test_inputs = pdpd_executor.generate_inputs(batch_size)
+
+        ## paddle predictor inference
+        pdpd_predict_executor = PaddlePredictorExecutor(str(model_file), str(model_params_file))
+        pdpd_predict_executor.run(test_inputs, warmup, repeats)
+        pdpd_predict_result = pdpd_predict_executor.get_inference_results()
+        paddle_warmup_frame_time = pdpd_predict_executor.warmup_time
+        paddle_repeats_per_frame_time = pdpd_predict_executor.repeat_time
+
+        if result_level<=0:
+            return
+
+        ## openvino inference
+        ov_executor = OpenvinoExecutor(model_file, 10, openvino_api_type)
+        ov_executor.run(test_inputs, warmup, repeats)
+        ov_result = ov_executor.get_inference_results()
+        openvino_warmup_frame_time = ov_executor.warmup_time
+        openvino_repeats_per_frame_time = ov_executor.repeat_time
+        result_level = 2
+
+        # compare
+        ov_result = [ov_result[k] for k in sorted(ov_result)]
+        pdpd_predict_result = [pdpd_predict_result[k] for k in sorted(pdpd_predict_result)]
+        res = compare(ov_result, pdpd_predict_result)
+
+        if res:
+            result_level = 3
+
+    finally:
+        timestamps = [paddle_warmup_frame_time, paddle_repeats_per_frame_time, openvino_warmup_frame_time, openvino_repeats_per_frame_time]
+        print(timestamps)
+        timestamps = ["{:.2f}".format(t) if t is not None else 'NONE' for t in timestamps]
+        # write append to result_save_file
+        with open(result_save_file, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow([result_prefix_str, result_level_dict[result_level]] + timestamps)
+
+
+"""
+pytest test cases
+"""
 
 def create_params_list_by_csv_file(csv_file, model_category_path, result_save_file, batch_size=1, warmup=0, repeats=1, openvino_api_type='sync'):
     Params=namedtuple('Params', 'result_prefix_str, model_file, model_params_file, result_save_file, result_level, batch_size, warmup, repeats, openvino_api_type')
